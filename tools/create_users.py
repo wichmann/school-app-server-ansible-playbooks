656d2345bdbccd48fdfc7a80576df6dc15133a7e
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2024-10-17 13:54:48 +0200

    Provide Python scripts for creating users in Authentik via API.

---
+++ tools/create_users.py
@@ -0,0 +1,117 @@
+
+"""
+Create users in Authentik from a given list as CSV file.
+
+Requirements:
+ - requests
+
+Sources:
+ - https://docs.goauthentik.io/docs/developer-docs/api/reference/core-users-create
+ - https://docs.goauthentik.io/docs/developer-docs/api/reference/core-users-set-password-create
+"""
+
+import json
+
+import yaml
+import requests
+
+
+BASE_URL = 'https://auth.xxx.com'
+
+
+def import_user_list():
+    import csv
+    with open('user_list.csv', newline='') as f:
+        reader = csv.reader(f)
+        result = [list((e.strip() for e in row)) for row in reader]
+    return result
+
+
+def load_API_token():
+    with open('../group_vars/all.yml', 'r') as file:
+        ansible_group_vars = yaml.safe_load(file)
+        TOKEN = ansible_group_vars['services']['authentik']['token']
+        print(f'Loaded token from Ansible group variables: {TOKEN}')
+    return TOKEN
+
+TOKEN = load_API_token()
+
+def create_user(name, username, email, groups=()):
+    print(f'Creating user {username}...')
+    url = f'{BASE_URL}/api/v3/core/users/'
+    payload = json.dumps({
+        'username': username,
+        'name': name,
+        'is_active': True,
+        'email': email,
+        'type': 'internal'
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {TOKEN}'
+    }
+    response = requests.request('POST', url, headers=headers, data=payload)
+    #print(response.text)
+    print(f'Created user {username} with email {email} and assigned user id {response.json()["pk"]}')
+    return response.json()["pk"]
+
+
+def set_user_password(userid, password):
+    print(f'Setting password for user no. {userid}...')
+    url = f'{BASE_URL}/api/v3/core/users/{userid}/set_password/'
+    payload = json.dumps({
+        'password': password
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {TOKEN}'
+    }
+    response = requests.request('POST', url, headers=headers, data=payload)
+    print(response.text)
+
+
+def create_group(name):
+    print(f'Creating group {name}...')
+    url = f'{BASE_URL}/api/v3/core/groups/'
+    payload = json.dumps({
+        'name': name,
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {TOKEN}'
+    }
+    response = requests.request('POST', url, headers=headers, data=payload)
+    print(response.text)
+
+
+def list_users():
+    print('Listing users...')
+    url = f'{BASE_URL}/api/v3/core/users/'
+    payload = {}
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {TOKEN}'
+    }
+    response = requests.request('GET', url, headers=headers, data=payload)
+    print(json.dumps(response.json(), indent=4))
+
+
+def list_groups():
+    print('Listing groups...')
+    url = f'{BASE_URL}/api/v3/core/groups/'
+    payload = {}
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {TOKEN}'
+    }
+    response = requests.request('GET', url, headers=headers, data=payload)
+    print(json.dumps(response.json(), indent=4))
+
+
+#list_users()
+#list_groups()
+for firstname, surname, email, username, password, group in import_user_list():
+    userid = create_user(f'{firstname} {surname}', username, email)
+    set_user_password(userid, password)
---
+++ tools/create_users2.py
@@ -0,0 +1,78 @@
+
+"""
+Create users in Authentik from a given list as CSV file.
+
+Requirements:
+ - authentik_client
+
+Source:
+ - https://pypi.org/project/authentik-client/
+"""
+
+
+import csv
+import yaml
+
+import authentik_client
+from authentik_client.rest import ApiException
+
+
+def import_user_list_csv():
+    with open('user_list.csv', newline='') as f:
+        reader = csv.reader(f)
+        result = [list((e.strip() for e in row)) for row in reader]
+    return result
+
+
+def load_API_token():
+    with open('../group_vars/all.yml', 'r') as file:
+        ansible_group_vars = yaml.safe_load(file)
+        TOKEN = ansible_group_vars['services']['authentik']['token']
+        print(f'Loaded token from Ansible group variables: {TOKEN}')
+    return TOKEN
+
+
+configuration = authentik_client.Configuration(
+    host='https://auth.xxx.com/api/v3',
+    access_token=load_API_token()
+)
+
+
+def list_users(core_api):
+    api_response = core_api.core_users_list()
+    print('The response of core_users_list:')
+    for u in api_response.results:
+        print(u.username, u.name, u.email)
+
+
+def create_user(core_api, name, username, email):
+    print(f'Creating user {username}...')
+    api_response = core_api.core_users_create(
+        user_request={'name': name, 'username': username, 'email': email}
+    )
+    #print(api_response)
+    print(f'Created user {api_response.username} with email {api_response.email} and assigned user id {api_response.pk}')
+    return api_response.pk
+
+
+def set_user_password(core_api, userid, password):
+    print(f'Setting password for user no. {userid}...')
+    api_response = core_api.core_users_set_password_create(
+        id=userid, user_password_set_request={'password': password}
+    )
+    #print(api_response)
+
+
+def main():
+    with authentik_client.ApiClient(configuration) as api_client:
+        core_api = authentik_client.CoreApi(api_client)
+        try:
+            # list_users(core_api)
+            for firstname, surname, email, username, password, group in import_user_list_csv():
+                userid = create_user(core_api, f'{firstname} {surname}', username, email)
+                set_user_password(core_api, userid, password)
+        except ApiException as e:
+            print('Exception when calling AdminApi->admin_apps_list: %s\n' % e)
+
+
+main()

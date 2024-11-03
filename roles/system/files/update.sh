b2e9cba3e3224ec5567175c538d411aa8ac20a06
Christian Wichmann <wichmann@bbs-os-brinkstr.de>
2024-11-03 08:39:38 +0100

    Improve update script and set scripts directory in group variable.

--- roles/system/files/update.sh
+++ roles/system/files/update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# Update des gesamten Servers
-# Autor: Christian Wichmann
-# Datum: 2024-06-06
+# Manual update of all packages and Docker container
+# Author: Christian Wichmann
+# Date: 2024-11-03
 
 # Check if user is root (https://stackoverflow.com/a/18216122/18073555)
 if [ "$EUID" -ne 0 ]
@@ -11,10 +11,37 @@ if [ "$EUID" -ne 0 ]
 fi
 
 echo
-echo "*** Aktualisiere Pakete und Docker Container... ***"
+echo "*** Update all packages and Docker container manually... ***"
 echo
 
 apt update
 apt full-upgrade -y
+
+docker compose -f container/infrastructure/docker-compose.yml pull
+docker compose -f container/infrastructure/docker-compose.yml up -d
+
+docker compose -f container/identity-provider/docker-compose.yml pull
+docker compose -f container/identity-provider/docker-compose.yml up -d
+
+#docker compose -f container/kanboard/docker-compose.yml pull
+#docker compose -f container/kanboard/docker-compose.yml up -d
+
+#docker compose -f container/jenkins/docker-compose.yml pull
+#docker compose -f container/jenkins/docker-compose.yml up -d
+
+#docker compose -f container/gitea/docker-compose.yml pull
+#docker compose -f container/gitea/docker-compose.yml up -d
+
+docker compose -f container/etherpad/docker-compose.yml pull
+docker compose -f container/etherpad/docker-compose.yml up -d
+
+docker compose -f container/hedgedoc/docker-compose.yml pull
+docker compose -f container/hedgedoc/docker-compose.yml up -d
+
+docker compose -f container/onlyoffice/docker-compose.yml pull
+docker compose -f container/onlyoffice/docker-compose.yml up -d
+
+docker compose -f container/homepage/docker-compose.yml pull
+docker compose -f container/homepage/docker-compose.yml up -d
 
 #docker image prune -a -f
